f041f4b4ee33d34ad59a632c6ea45b19b2569f3a
gabuladze <itisabu@gmail.com>
2017-12-07 23:54:12 +0400

    Display borrowed items in first tab & added search

--- LibraryForms/StudentForm.Designer.cs
+++ LibraryForms/StudentForm.Designer.cs
@@ -33,32 +33,31 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.messageLabel = new System.Windows.Forms.Label();
-            this.borrowButton = new System.Windows.Forms.Button();
-            this.allItemsGridView = new System.Windows.Forms.DataGridView();
-            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.slugDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.displaynameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.authoridDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.authorsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.librarydbDataSet = new LibraryForms.librarydbDataSet();
-            this.categoryidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.categoriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.genreidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.categoriesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.lendablesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.lendablesTableAdapter = new LibraryForms.librarydbDataSetTableAdapters.lendablesTableAdapter();
             this.authorsTableAdapter = new LibraryForms.librarydbDataSetTableAdapters.authorsTableAdapter();
             this.categoriesTableAdapter = new LibraryForms.librarydbDataSetTableAdapters.categoriesTableAdapter();
             this.borrowedItemListView = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resultsListView = new System.Windows.Forms.ListView();
+            this.searchLendableButton = new System.Windows.Forms.Button();
+            this.searchValueTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.allItemsGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.authorsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.librarydbDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lendablesBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -85,9 +84,13 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.searchLendableButton);
+            this.tabPage2.Controls.Add(this.searchValueTextBox);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Controls.Add(this.messageLabel);
-            this.tabPage2.Controls.Add(this.borrowButton);
-            this.tabPage2.Controls.Add(this.allItemsGridView);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -104,73 +107,6 @@
             this.messageLabel.Size = new System.Drawing.Size(0, 13);
             this.messageLabel.TabIndex = 2;
             // 
-            // borrowButton
-            // 
-            this.borrowButton.Location = new System.Drawing.Point(703, 512);
-            this.borrowButton.Name = "borrowButton";
-            this.borrowButton.Size = new System.Drawing.Size(75, 23);
-            this.borrowButton.TabIndex = 1;
-            this.borrowButton.Text = "BORROW";
-            this.borrowButton.UseVisualStyleBackColor = true;
-            // 
-            // allItemsGridView
-            // 
-            this.allItemsGridView.AllowUserToAddRows = false;
-            this.allItemsGridView.AllowUserToDeleteRows = false;
-            this.allItemsGridView.AutoGenerateColumns = false;
-            this.allItemsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.allItemsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.idDataGridViewTextBoxColumn,
-            this.slugDataGridViewTextBoxColumn,
-            this.displaynameDataGridViewTextBoxColumn,
-            this.authoridDataGridViewTextBoxColumn,
-            this.categoryidDataGridViewTextBoxColumn,
-            this.genreidDataGridViewTextBoxColumn});
-            this.allItemsGridView.DataSource = this.lendablesBindingSource;
-            this.allItemsGridView.Dock = System.Windows.Forms.DockStyle.Top;
-            this.allItemsGridView.Location = new System.Drawing.Point(3, 3);
-            this.allItemsGridView.Name = "allItemsGridView";
-            this.allItemsGridView.ReadOnly = true;
-            this.allItemsGridView.Size = new System.Drawing.Size(778, 503);
-            this.allItemsGridView.TabIndex = 0;
-            // 
-            // idDataGridViewTextBoxColumn
-            // 
-            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
-            this.idDataGridViewTextBoxColumn.Frozen = true;
-            this.idDataGridViewTextBoxColumn.HeaderText = "ID";
-            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
-            this.idDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // slugDataGridViewTextBoxColumn
-            // 
-            this.slugDataGridViewTextBoxColumn.DataPropertyName = "slug";
-            this.slugDataGridViewTextBoxColumn.Frozen = true;
-            this.slugDataGridViewTextBoxColumn.HeaderText = "Slug";
-            this.slugDataGridViewTextBoxColumn.Name = "slugDataGridViewTextBoxColumn";
-            this.slugDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // displaynameDataGridViewTextBoxColumn
-            // 
-            this.displaynameDataGridViewTextBoxColumn.DataPropertyName = "display_name";
-            this.displaynameDataGridViewTextBoxColumn.Frozen = true;
-            this.displaynameDataGridViewTextBoxColumn.HeaderText = "Display Name";
-            this.displaynameDataGridViewTextBoxColumn.Name = "displaynameDataGridViewTextBoxColumn";
-            this.displaynameDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // authoridDataGridViewTextBoxColumn
-            // 
-            this.authoridDataGridViewTextBoxColumn.DataPropertyName = "author_id";
-            this.authoridDataGridViewTextBoxColumn.DataSource = this.authorsBindingSource;
-            this.authoridDataGridViewTextBoxColumn.DisplayMember = "first_name";
-            this.authoridDataGridViewTextBoxColumn.Frozen = true;
-            this.authoridDataGridViewTextBoxColumn.HeaderText = "Author";
-            this.authoridDataGridViewTextBoxColumn.Name = "authoridDataGridViewTextBoxColumn";
-            this.authoridDataGridViewTextBoxColumn.ReadOnly = true;
-            this.authoridDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.authoridDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.authoridDataGridViewTextBoxColumn.ValueMember = "id";
-            // 
             // authorsBindingSource
             // 
             this.authorsBindingSource.DataMember = "authors";
@@ -181,36 +117,10 @@
             this.librarydbDataSet.DataSetName = "librarydbDataSet";
             this.librarydbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
-            // categoryidDataGridViewTextBoxColumn
-            // 
-            this.categoryidDataGridViewTextBoxColumn.DataPropertyName = "category_id";
-            this.categoryidDataGridViewTextBoxColumn.DataSource = this.categoriesBindingSource;
-            this.categoryidDataGridViewTextBoxColumn.DisplayMember = "display_name";
-            this.categoryidDataGridViewTextBoxColumn.Frozen = true;
-            this.categoryidDataGridViewTextBoxColumn.HeaderText = "Category";
-            this.categoryidDataGridViewTextBoxColumn.Name = "categoryidDataGridViewTextBoxColumn";
-            this.categoryidDataGridViewTextBoxColumn.ReadOnly = true;
-            this.categoryidDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.categoryidDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.categoryidDataGridViewTextBoxColumn.ValueMember = "id";
-            // 
             // categoriesBindingSource
             // 
             this.categoriesBindingSource.DataMember = "categories";
             this.categoriesBindingSource.DataSource = this.librarydbDataSet;
-            // 
-            // genreidDataGridViewTextBoxColumn
-            // 
-            this.genreidDataGridViewTextBoxColumn.DataPropertyName = "genre_id";
-            this.genreidDataGridViewTextBoxColumn.DataSource = this.categoriesBindingSource1;
-            this.genreidDataGridViewTextBoxColumn.DisplayMember = "display_name";
-            this.genreidDataGridViewTextBoxColumn.Frozen = true;
-            this.genreidDataGridViewTextBoxColumn.HeaderText = "Genre";
-            this.genreidDataGridViewTextBoxColumn.Name = "genreidDataGridViewTextBoxColumn";
-            this.genreidDataGridViewTextBoxColumn.ReadOnly = true;
-            this.genreidDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.genreidDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.genreidDataGridViewTextBoxColumn.ValueMember = "id";
             // 
             // categoriesBindingSource1
             // 
@@ -244,6 +154,73 @@
             this.borrowedItemListView.UseCompatibleStateImageBehavior = false;
             this.borrowedItemListView.View = System.Windows.Forms.View.Details;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resultsListView);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(3, 85);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(778, 468);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RESULTS";
+            // 
+            // resultsListView
+            // 
+            this.resultsListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultsListView.Location = new System.Drawing.Point(3, 16);
+            this.resultsListView.Name = "resultsListView";
+            this.resultsListView.Size = new System.Drawing.Size(772, 449);
+            this.resultsListView.TabIndex = 0;
+            this.resultsListView.UseCompatibleStateImageBehavior = false;
+            this.resultsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // searchLendableButton
+            // 
+            this.searchLendableButton.Location = new System.Drawing.Point(679, 61);
+            this.searchLendableButton.Name = "searchLendableButton";
+            this.searchLendableButton.Size = new System.Drawing.Size(75, 23);
+            this.searchLendableButton.TabIndex = 12;
+            this.searchLendableButton.Text = "SEARCH";
+            this.searchLendableButton.UseVisualStyleBackColor = true;
+            this.searchLendableButton.Click += new System.EventHandler(this.searchLendableButton_Click);
+            // 
+            // searchValueTextBox
+            // 
+            this.searchValueTextBox.Location = new System.Drawing.Point(6, 63);
+            this.searchValueTextBox.Name = "searchValueTextBox";
+            this.searchValueTextBox.Size = new System.Drawing.Size(667, 20);
+            this.searchValueTextBox.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "SEARCH FOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "very";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 26);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ADVANCED SEARCH";
+            // 
             // StudentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -258,12 +235,12 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.allItemsGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.authorsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.librarydbDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.lendablesBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -274,8 +251,6 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Label messageLabel;
-        private System.Windows.Forms.Button borrowButton;
-        private System.Windows.Forms.DataGridView allItemsGridView;
         private librarydbDataSet librarydbDataSet;
         private System.Windows.Forms.BindingSource lendablesBindingSource;
         private librarydbDataSetTableAdapters.lendablesTableAdapter lendablesTableAdapter;
@@ -284,12 +259,13 @@
         private System.Windows.Forms.BindingSource categoriesBindingSource;
         private librarydbDataSetTableAdapters.categoriesTableAdapter categoriesTableAdapter;
         private System.Windows.Forms.BindingSource categoriesBindingSource1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn slugDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn displaynameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewComboBoxColumn authoridDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewComboBoxColumn categoryidDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewComboBoxColumn genreidDataGridViewTextBoxColumn;
         private System.Windows.Forms.ListView borrowedItemListView;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView resultsListView;
+        private System.Windows.Forms.Button searchLendableButton;
+        private System.Windows.Forms.TextBox searchValueTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- LibraryForms/StudentForm.cs
+++ LibraryForms/StudentForm.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Linq.SqlClient;
 
 namespace LibraryForms
 {
     public partial class StudentForm : Form
     {
         List<Lendable> lendables;
+        List<Lendable> searchLendables;
         LibraryDBDataContext LibraryDB = new LibraryDBDataContext();
         User currentUser;
 
@@ -30,20 +32,28 @@ namespace LibraryForms
             this.authorsTableAdapter.Fill(this.librarydbDataSet.authors);
             // TODO: This line of code loads data into the 'librarydbDataSet.lendables' table. You can move, or remove it, as needed.
             this.lendablesTableAdapter.Fill(this.librarydbDataSet.lendables);
-            this.addColumnsToListView();
+            this.addColumnsToListViews();
             this.fetchBorrowedItems();
         }
 
-        private void addColumnsToListView()
+        private void addColumnsToListViews()
         {
-            ColumnHeader[] cols = new ColumnHeader[] { 
+            borrowedItemListView.Columns.AddRange(
+                new ColumnHeader[] { 
                         new ColumnHeader { Text = "Name", Width = 120 },
                         new ColumnHeader { Text = "Author",Width = 100 },
                         new ColumnHeader { Text = "Category", Width = 100 }, 
                         new ColumnHeader { Text = "Genre", Width = 100 }
-                    };
-
-            borrowedItemListView.Columns.AddRange(cols);
+                    });
+            
+            resultsListView.Columns.AddRange(
+                new ColumnHeader[] { 
+                        new ColumnHeader { Text = "Name", Width = 120 },
+                        new ColumnHeader { Text = "Author",Width = 100 },
+                        new ColumnHeader { Text = "Category", Width = 100 }, 
+                        new ColumnHeader { Text = "Genre", Width = 100 },
+                        new ColumnHeader { Text = "Free", Width = 100 }
+                    });
         }
 
         private void fetchBorrowedItems()
@@ -104,6 +114,77 @@ namespace LibraryForms
                     borrowedItemListView.Items.Add(i1); 
                 }
 	        }
+        }
+
+        private void searchLendableButton_Click(object sender, EventArgs e)
+        {
+            //validate search value
+            if (searchValueTextBox.TextLength > 0)
+            {
+                try
+                {
+                    this.searchLendables = (
+                            from l in LibraryDB.lendables
+                            join a in LibraryDB.authors on l.author_id equals a.id
+                            join c in LibraryDB.categories on l.category_id equals c.id
+                            join g in LibraryDB.genres on l.genre_id equals g.id
+                            join lu in LibraryDB.lendable_users on l.id equals lu.lendable_id into finalGroup
+                            from finalItem in finalGroup.DefaultIfEmpty()
+                            where SqlMethods.Like(l.display_name, "%" + searchValueTextBox.Text + "%")
+                            select new Lendable
+                            {
+                                id = l.id,
+                                slug = l.slug,
+                                display_name = l.display_name,
+                                author = new Author
+                                {
+                                    id = a.id,
+                                    first_name = a.first_name,
+                                    last_name = a.last_name
+                                },
+                                category = new Category
+                                {
+                                    id = c.id,
+                                    slug = c.slug,
+                                    display_name = c.display_name
+                                },
+                                genre = new Genre
+                                {
+                                    id = g.id,
+                                    slug = g.slug,
+                                    display_name = g.display_name
+                                },
+                                user = new User
+                                {
+                                    id = finalItem.user_id != null ? finalItem.user_id : 0
+                                }
+                            }
+                        ).ToList<Lendable>();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("No lendables have been found");
+                    throw;
+                }
+
+                if (this.lendables != null)
+                {
+                    resultsListView.Items.Clear();
+
+                    foreach (var item in this.searchLendables)
+                    {
+                        ListViewItem i1 = new ListViewItem(item.display_name);
+                        i1.SubItems.Add(item.author.first_name + " " + item.author.last_name);
+                        i1.SubItems.Add(item.category.display_name);
+                        i1.SubItems.Add(item.genre.display_name);
+                        i1.SubItems.Add(item.user.id != 0 ? "NO" : "YES");
+
+                        resultsListView.Items.Add(i1);
+                    }
+                }
+
+            }
+
         }
     }
 }
